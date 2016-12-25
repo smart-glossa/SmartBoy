9ad66e5d29133a1f5e61be127cc3e9883b9ccfdf
MuthuBarathi1998 <muthubharathi@smartglossa.com>
2016-12-25 11:38:35 +0530

    add alphabets

--- smartboy/src/main/webapp/js/smartBoy.js
+++ smartboy/src/main/webapp/js/smartBoy.js
@@ -1,9 +1,358 @@
-function alphabets(){
+function alphabetsa(){
 	var strVar="";
 	strVar += "<div id=\"a\">";
 	strVar += "<h1>Alphabet Letters<\/h1>";
 	strVar += "<p>A (a)<\/p>";
-	strVar += "<div id=\"imga><img src=\"/images\/apple1.jpg\" id=\"imgapp\" class=\"\">";
+	strVar += "<div id=\"imga><img src=\"/images\/a.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsb(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/b.png\" id=\"\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsc(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/c.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsd(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/d.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetse(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/e.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsf(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/f.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsg(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/g.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsh(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/h.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsi(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/i.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsj(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/j.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsk(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/k.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsl(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/l.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsm(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/m.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsn(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/n.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetso(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/o.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}function alphabetsp(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/p.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsq(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/q.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsr(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/r.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetss(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/s.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetst(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/t.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsu(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/u.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsv(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/v.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsw(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/w.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsx(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/x.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsy(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/y.png\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
+function alphabetsz(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/z.png\" id=\"imgapp\" class=\"\">";
 	strVar += "<button id=\"na\">next<\/button>";
 	strVar += "<button id=\"ba\">back<\/button>";
 	strVar += "<\/div>";
