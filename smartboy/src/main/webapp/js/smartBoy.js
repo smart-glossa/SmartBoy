e9e001ab3f576cae8a8adcd44aa92a32da32a58d
MuthuBarathi1998 <muthubharathi@smartglossa.com>
2016-12-25 11:24:45 +0530

    add a function

--- smartboy/src/main/webapp/js/smartBoy.js
+++ smartboy/src/main/webapp/js/smartBoy.js
@@ -1,3 +1,14 @@
-/**
- * 
- */
+function alphabets(){
+	var strVar="";
+	strVar += "<div id=\"a\">";
+	strVar += "<h1>Alphabet Letters<\/h1>";
+	strVar += "<p>A (a)<\/p>";
+	strVar += "<div id=\"imga><img src=\"/images\/apple1.jpg\" id=\"imgapp\" class=\"\">";
+	strVar += "<button id=\"na\">next<\/button>";
+	strVar += "<button id=\"ba\">back<\/button>";
+	strVar += "<\/div>";
+
+	$(".second")[0].innerHTML = strVar;
+
+	
+}
